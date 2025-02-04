a964f5f45d46de464838691e9e2c9e59cfaf5164
The-HSQ <huaali7860@gmail.com>
2025-02-04 17:46:45 +0500

    UpdateloginSchema.jsx
    Login Schema changes

--- src/components/schema/loginSchema.jsx
+++ src/components/schema/loginSchema.jsx
@@ -1,6 +1,6 @@
 import * as yup from "yup";
 
 export const loginSchema = yup.object().shape({
-    username: yup.string().required("Username is required"),
+    email: yup.string().email("Invalid email address").required("Email is required"),
     password: yup.string().required("Password is required"),
 });
